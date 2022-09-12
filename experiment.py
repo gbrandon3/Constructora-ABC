b68a61712ceee1ab64759b1abc2fc0b43fa34649
Brandon <b.gonzalezp@uniandes.edu.co>
2022-09-11 20:40:39 -0500

    gateway

---
+++ experiment.py
@@ -0,0 +1,22 @@
+import time
+import requests
+import random
+
+request=100
+fallos=0
+with open('resultado.txt', 'w') as f:
+    for i in range(request):
+       
+        error=random.randint(0, 1)==1
+        data={} if random.randint(0, 1)==1 else {"accion":"crear"}
+        start=time.time()
+        request=requests.post("http://127.0.0.1:5000/gateway/crear",json=data)
+   
+        end=time.time()
+        resul=" Se creo la regla "if request.status_code==200 else "No se creo la regla"
+        if(end-start>=4):
+            fallos+=1
+        print("Request "+str(i+1)+ resul+' tiempo: '+str(end-start)+"\n")
+        f.write('tiempo:'+str(end-start)+"\n"+"del request "+str(i+1)+ resul+' tiempo: '+str(end-start)+"\n")
+        time.sleep(1)
+print("No cumplio "+str(fallos)+" veces")
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -3,20 +3,35 @@ from urllib import request
 from flask import request
 from flask_restful import Resource
 import requests
-
-
+import json
+import time
+import random
 class VistaGateway(Resource):
     def post(self):
+    
+        
         peticion = requests.get(
             "https://experimientoi-apimanagement.azure-api.net/servicio/ubicacion/ubicacion")
-
+       
+        
         if(peticion.status_code == 200):
-
-           
-         
+             
             peticion2 = requests.post(
-                "https://experimientoi-apimanagement.azure-api.net/servicio/regla/reglas")
-            print(peticion2)
+                "https://experimientoi-apimanagement.azure-api.net/servicio/regla/reglas",json=request.json)
+            
+            if(peticion2.status_code!=200):
+         
+                peticion3=requests.post("https://experimientoi-apimanagement.azure-api.net/servicio/duplicado/reglas",json={"accion":"crear"})
+                time.sleep(random.uniform(0.5,1.0))
+                if(peticion3.status_code==200):
+                    return {"mensaje":"se creo la regla"},'200'
+                else:
+                    return {"mensaje":"No se creo la regla"},'500'
+            else:
+                return {"mensaje":"se creo la regla"},'200'
+            
+             
+          
 
                 
         
