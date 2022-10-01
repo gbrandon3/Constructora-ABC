48483d9131037c273af012833db732c556a2de00
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2022-10-01 11:29:05 -0500

    Se realizan ajuste en el servicio de autorización, se cambia la identidad asignada.

--- experimento 2/servicios/autorizador/vistas/vistas.py
+++ experimento 2/servicios/autorizador/vistas/vistas.py
@@ -1,12 +1,13 @@
-
-from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token,jwt_required
+from flask_jwt_extended import create_access_token, jwt_required
+
 
 class VistaAutorizador(Resource):
+
     def post(self):
-        token_de_acceso = create_access_token(identity="UsuarioPrueba", additional_claims={"tipo": 2})
-        return {"token":token_de_acceso}
+        token_de_acceso = create_access_token(identity="1090356984")
+        return {"token": token_de_acceso}
+
     @jwt_required()
     def get(self):
-        return {"Autorizado":True}
+        return {"Autorizado": True}
