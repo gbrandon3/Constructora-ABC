65654fccd5fbc979b50bed470ec074705ec322ad
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 11:38:41 -0500

    Servicio autorizador

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import create_access_token,jwt_required
 
 class VistaAutorizador(Resource):
     def post(self):
-        token_de_acceso = create_access_token(identity="UsuarioPrueba", additional_claims={"tipo": 2})
+        token_de_acceso = create_access_token(identity="1090356984")
         return {"token":token_de_acceso}
     @jwt_required()
     def get(self):
