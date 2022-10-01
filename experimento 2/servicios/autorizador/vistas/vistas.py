b1e1c80637f17b55339d0b27fa4fa8cbb4c229de
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 10:29:30 -0500

    Autorizador terminado

--- experimento 2/servicios/autorizador/vistas/vistas.py
+++ experimento 2/servicios/autorizador/vistas/vistas.py
@@ -1,10 +1,12 @@
 
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token,jwt_required
 
 class VistaAutorizador(Resource):
     def post(self):
         token_de_acceso = create_access_token(identity="UsuarioPrueba", additional_claims={"tipo": 2})
         return {"token":token_de_acceso}
-        
+    @jwt_required()
+    def get(self):
+        return {"Autorizado":True}
