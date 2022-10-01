2730f7eca19abeffac60a6f23b8bcc92a1c2e451
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 11:11:22 -0500

    Servicio autorizador

--- experimento 1/README.md
+++
@@ -1,7 +0,0 @@
-# Constructora-ABC-grupo05
-# Ejecucion
-- Ejecutar el comando pip install -r requirements.txt
-- Ejecutar el comando en la raiz del proyecto
-- Ejecutar el archivo experiment.py
-
-
--- experimento 2/servicios/ubicacion/vistas/vistas.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-from flask_jwt_extended import jwt_required
-class VistaUbicacion(Resource):
-    @jwt_required()
-    def get(self):
-        return{"ubicacion":{"id":1}}
