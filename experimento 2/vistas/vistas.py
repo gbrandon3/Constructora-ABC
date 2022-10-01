5a93109514371c1dbb393534253654189132230a
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2022-10-01 11:14:24 -0500

    Se realizan ajustes en: * vistas.py * app.py * experiment.py

--- experimento 2/vistas/vistas.py
+++ experimento 2/vistas/vistas.py
@@ -1,30 +1,16 @@
-from multiprocessing import reduction
 from urllib import request
 from flask import request
 from flask_restful import Resource
 import requests
-import json
-import time
-import random
+
+
 class VistaGateway(Resource):
     def post(self):
-    
-        
         peticion = requests.get(
             "https://experimientoi-apimanagement.azure-api.net/servicio/ubicacion/ubicacion")
-       
-        
-        if(peticion.status_code == 200):
-             
+
+        if (peticion.status_code == 200):
             peticion2 = requests.post(
-                "https://experimientoi-apimanagement.azure-api.net/servicio/regla/reglas",json=request.json)
-            
-        return {"mensaje":"se creo la regla"},'200'
-            
-             
-          
+                "https://experimientoi-apimanagement.azure-api.net/servicio/regla/reglas", json=request.json)
 
-                
-        
-
-        
+        return {"mensaje": "se creo la regla"}, '200'
