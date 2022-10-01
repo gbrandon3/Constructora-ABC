b9cde4a2aafd169eb77848c22ae568481ce662a3
Esteban Ardila <111327245+ceardilav@users.noreply.github.com>
2022-10-01 14:13:39 -0500

    Update vistas.py

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -2,15 +2,16 @@
 from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
+import random
 class VistaAutorizador(Resource):
     def post(self):
-        token_de_acceso = create_access_token(identity="1090356984")
+        numbers = [1,2]
+        token_de_acceso = create_access_token(random.choice(number))
         return {"token":token_de_acceso}
     
     @jwt_required()
     def get(self):
         try:
-            request_identity = get_jwt_identity()
-            return { "Autorizado": request_identity == "1090356984" }, 200
+            return { "Autorizado": True }, 200
         except:
             return { "Autorizado": False }, 401
