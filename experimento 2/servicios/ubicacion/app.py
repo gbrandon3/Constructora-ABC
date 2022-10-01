c9f09a5b055e8f6f876f211554f5479641ad67b5
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 10:45:19 -0500

    JWT

--- experimento 2/servicios/ubicacion/app.py
+++ experimento 2/servicios/ubicacion/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
-
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from vistas import VistaUbicacion
 app = Flask('ubicacion')
 
 
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app_context=app.app_context()
 app_context.push()
 cors = CORS(app)
 api=Api(app)
 api.add_resource(VistaUbicacion,"/ubicacion")
+jwt = JWTManager(app)
--- experimento 2/servicios/ubicacion/vistas/vistas.py
+++ experimento 2/servicios/ubicacion/vistas/vistas.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
-
+from flask_jwt_extended import jwt_required
 class VistaUbicacion(Resource):
+    @jwt_required()
     def get(self):
         return{"ubicacion":{"id":1}}
