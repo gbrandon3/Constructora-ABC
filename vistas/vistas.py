3076eb708faffb730a97535873b4d450697d8192
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 15:17:57 -0500

    Ajustes ataques

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -4,11 +4,19 @@ from flask_restful import Resource
 from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
 import random
 class VistaAutorizador(Resource):
+
     def post(self):
-        numbers = [1,2]
-        token_de_acceso = create_access_token(random.choice(numbers))
-        return {"token":token_de_acceso}
-    
+        if(request.json["usuario"]=="usuarioPrueba"  or request.json["usuario"]=="usuarioPrueba2" and request.json["password"]=="pass"):
+            if(request.json["usuario"]=="usuarioPrueba" and request.json["ip"]=="141.250.247.54"):
+                token_de_acceso = create_access_token(1)
+                return {"token":token_de_acceso}
+            elif(request.json["usuario"]=="usuarioPrueba2" ):
+                token_de_acceso = create_access_token(2)
+                return {"token":token_de_acceso}
+            else:
+                return {"mensaje":"Acceso denegado"},401
+        else:
+            return {"mensaje":"Acceso denegado"},401
     @jwt_required()
     def get(self):
         try:
