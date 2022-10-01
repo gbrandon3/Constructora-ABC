87f26d5ed7c5921058e960006cbe21c309b1b715
Brandon <b.gonzalezp@uniandes.edu.co>
2022-10-01 12:11:21 -0500

    Servicio autorizador

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,8 +1,7 @@
 
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token,jwt_required
-
+from flask_jwt_extended import create_access_token,jwt_required,get_jwt_identity
 class VistaAutorizador(Resource):
     def post(self):
         token_de_acceso = create_access_token(identity="1090356984")
@@ -11,4 +10,4 @@ class VistaAutorizador(Resource):
     @jwt_required()
     def get(self):
         request_identity = get_jwt_identity()
-        return { "Autorizado": reques_identity == "1090356984" }, 200
+        return { "Autorizado": request_identity == "1090356984" }, 200
