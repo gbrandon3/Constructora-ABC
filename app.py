406bab0aeca7104ff86ebc3176ac50cd55f58415
Esteban Ardila <111327245+ceardilav@users.noreply.github.com>
2022-10-01 13:59:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
-
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from vistas import VistaReglas
+
 app = Flask('regla')
 
-
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app_context=app.app_context()
 app_context.push()
 cors = CORS(app)
-api=Api(app)
-api.add_resource(VistaReglas,"/reglas")
 
+api=Api(app)
+api.add_resource(VistaReglas,"/regla")
+
+jwt = JWTManager(app)
